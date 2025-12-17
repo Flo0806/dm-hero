1e1c0d9e3b122c162851664319852a47e0009070
Florian Heuberger <fh@flogersoft.de>
2025-12-17 20:16:40 +0100

    fix: remove trailing commas

--- packages/app/app/components/layout/NavigationDrawer.vue
+++ packages/app/app/components/layout/NavigationDrawer.vue
@@ -46,7 +46,7 @@
         prepend-icon="mdi-magnify"
         :title="$t('nav.search')"
         value="search"
-        :active ="isSearchActive"
+        :active="isSearchActive"
         @click="$emit('search-click')"
       />
       <v-list-item
@@ -149,8 +149,8 @@ interface Props {
   rail: boolean
   hasActiveCampaign: boolean
   activeCampaignName?: string | null
-  isDark: boolean,
-  isSearchActive: boolean,
+  isDark: boolean
+  isSearchActive: boolean
 }
 
 defineProps<Props>()
